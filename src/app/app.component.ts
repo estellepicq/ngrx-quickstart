db55653830251bd414caf12a5afa575381624107
estellepicq <33844394+estellepicq@users.noreply.github.com>
2018-07-09 22:38:03 +0200

    1st version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+interface AppState {
+  message: string;
+  post: Post;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  message$: Observable<string>;
+  post$: Observable<Post>;
+  text: string;
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message');
+    this.post$ = this.store.select('post');
+  }
+
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'})
+  }
+
+  frenchMessage() {
+    this.store.dispatch({type: 'FRENCH'})
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text) )
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset())
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote())
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote())
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
+import { postReducer } from './reducers/post.reducer';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      message: simpleReducer,
+      post: postReducer
+     })
   ],
   providers: [],
   bootstrap: [AppComponent]
